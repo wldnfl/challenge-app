172f212355efb9b19435bcf240e0b593049069fc
lis0517 <01034895017a@gmail.com>
2024-06-19 13:25:23 +0900

    init commit

---
+++ src/main/java/com/twelve/challegeapp/ChallegeAppApplication.java
@@ -0,0 +1,13 @@
+package com.twelve.challegeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallegeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallegeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/twelve/challegeapp/ChallegeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twelve.challegeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallegeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
