88ea3b217308cb4f4e2e92ea0e349c95b30dceff
lis0517 <01034895017a@gmail.com>
2024-06-20 04:05:55 +0900

    add UserController

---
+++ src/main/java/com/twelve/challengeapp/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.twelve.challengeapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.twelve.challengeapp.dto.UserRequestDto;
+import com.twelve.challengeapp.service.UserServiceImpl;
+import com.twelve.challengeapp.util.SuccessResponseFactory;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+	private final UserServiceImpl userService;
+
+	public UserController(UserServiceImpl userService) {
+		this.userService = userService;
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<?> registerUser(@RequestBody @Valid UserRequestDto.Register requestDto) {
+		userService.registerUser(requestDto);
+		return SuccessResponseFactory.ok();
+	}
+
+}
