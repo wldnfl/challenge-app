224e265b68dc2b587c4aa8c290f5b8c22e7c5628
jiwoo <1114jiwoo@gmail.com>
2024-06-20 17:57:49 +0900

    [fix] userId 추가

--- src/main/java/com/twelve/challengeapp/controller/PostController.java
+++ src/main/java/com/twelve/challengeapp/controller/PostController.java
@@ -2,11 +2,11 @@ package com.twelve.challengeapp.controller;
 
 import com.twelve.challengeapp.dto.PostRequestDto;
 import com.twelve.challengeapp.dto.PostResponseDto;
+import com.twelve.challengeapp.jwt.UserDetailsImpl;
 import com.twelve.challengeapp.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,29 +16,28 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public ResponseEntity<PostResponseDto> createPost(@RequestBody PostRequestDto requestDto, @AuthenticationPrincipal UserDetails userDetails) {
-        Long userId = Long.parseLong(userDetails.getUsername());
+    public ResponseEntity<PostResponseDto> createPost(@RequestBody PostRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long userId = userDetails.getUserId();
         PostResponseDto responseDto = postService.createPost(requestDto, userId);
         return ResponseEntity.ok(responseDto);
     }
 
-    @GetMapping
+    @GetMapping("/{postId}")
     public ResponseEntity<PostResponseDto> getPost(@PathVariable Long id) {
         return ResponseEntity.ok(postService.getPost(id));
     }
 
     @PutMapping("/{postId}")
-    public ResponseEntity<PostResponseDto> updatePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto, @AuthenticationPrincipal UserDetails userDetails) {
-        Long userId = Long.parseLong(userDetails.getUsername()); // assuming userId is stored as username
+    public ResponseEntity<PostResponseDto> updatePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long userId = userDetails.getUserId();
         PostResponseDto responseDto = postService.updatePost(id, requestDto, userId);
         return ResponseEntity.ok(responseDto);
     }
 
     @DeleteMapping("/{postId}")
-    public ResponseEntity<Void> deletePost(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails) {
-        Long userId = Long.parseLong(userDetails.getUsername());
+    public ResponseEntity<Void> deletePost(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long userId = userDetails.getUserId();
         postService.deletePost(id, userId);
         return ResponseEntity.noContent().build();
     }
-
 }
