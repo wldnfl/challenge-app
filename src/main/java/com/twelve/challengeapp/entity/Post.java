6b648f4e451cc67d85d8c772bd9b8e081f4a3011
lis0517 <01034895017a@gmail.com>
2024-06-21 04:09:25 +0900

    update Post
    User와 관계 설정

--- src/main/java/com/twelve/challengeapp/entity/Post.java
+++ src/main/java/com/twelve/challengeapp/entity/Post.java
@@ -1,41 +1,46 @@
 package com.twelve.challengeapp.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Table(name = "post")
 @Entity
 @Getter
 @NoArgsConstructor
-public class Post {
+public class Post extends Timestamped {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private Long userId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String title;
-    private String content;
+	private String title;
+	private String content;
 
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
 
-    @Builder
-    public Post(Long userId, String title, String content) {
-        this.userId = userId;
-        this.title = title;
-        this.content = content;
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
+	@Builder
+	public Post(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
 
-    public void update(String title, String content) {
-        this.title = title;
-        this.content = content;
-        this.updatedAt = LocalDateTime.now();
-    }
+	public void update(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+
+	void setUser(User user) {
+		this.user = user;
+	}
 }
