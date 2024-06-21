83c557fe33356a1f5c1088077699661b253bb4e9
lis0517 <01034895017a@gmail.com>
2024-06-21 15:01:38 +0900

    refactor JwtAuthenticationFilter
    doFilterInternal 정리
    try-catch로 예외 처리 추가
    ObjectMapper 멤버 변수로 추가

--- src/main/java/com/twelve/challengeapp/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/twelve/challengeapp/jwt/JwtAuthenticationFilter.java
@@ -7,10 +7,11 @@ import java.util.Map;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.twelve.challengeapp.service.JwtServiceImpl;
+import com.twelve.challengeapp.service.JwtService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -21,53 +22,55 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j(topic = "Jwt 검증 및 인가")
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-	private final UserDetailsServiceImpl userDetailsService;
-	private final JwtServiceImpl jwtService;
+	private final UserDetailsService userDetailsService;
+	private final JwtService jwtService;
+	private final ObjectMapper objectMapper;
 
-	public JwtAuthenticationFilter(JwtServiceImpl jwtService, UserDetailsServiceImpl userDetailsService) {
+	public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
 		this.jwtService = jwtService;
 		this.userDetailsService = userDetailsService;
+		objectMapper = new ObjectMapper();
 	}
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
-		String accessToken = jwtService.getAccessTokenFromRequest(request);
-		if (accessToken != null) {
-			if (jwtService.validateToken(accessToken)) {
-				// Access Token이 유효한 경우 인증 정보를 설정하고 다음 필터로 진행
-				log.info("Access Token 유효");
-				setAuthenticationContext(accessToken);
-				filterChain.doFilter(request, response);
-			} else {
-				// Access Token이 만료된 경우 Refresh Token을 사용하여 새로운 Access Token을 발급받음
-				String refreshToken = jwtService.getRefreshTokenFromRequest(request);
-				if (refreshToken != null && jwtService.validateToken(refreshToken)) {
-					log.info("Access Token 만료, Refresh Token 사용");
 
-					String username = jwtService.extractUsername(refreshToken);
-					Object role = jwtService.extractRole(refreshToken);
-
-					log.info(username + ", " + role);
-
-					String newAccessToken = jwtService.generateAccessToken(username, role);
-					setAuthenticationContext(newAccessToken);
-					jwtService.setHeaderWithAccessToken(response, newAccessToken);
-
-					filterChain.doFilter(request, response);
+		try {
+			String accessToken = jwtService.getAccessTokenFromRequest(request);
+			if (accessToken != null) {
+				if (jwtService.validateToken(accessToken)) {
+					log.info("Access Token 유효");
+					setAuthenticationContext(accessToken);
 				} else {
-					sendAuthenticationFailureResponse(response);
+					handleExpiredAccessToken(request, response);
 				}
 			}
-		} else {
-			// Access Token이 없는 경우 다음 필터로 진행
-			filterChain.doFilter(request, response);
+		} catch (Exception e) {
+			log.error("JWT error", e);
+			sendAuthenticationFailureResponse(response);
+			return;
 		}
+		filterChain.doFilter(request, response);
 	}
 
 	@Override
 	protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
 		return jwtService.isAuthorizationHeaderMissing(request);
+	}
+
+	private void handleExpiredAccessToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String refreshToken = jwtService.getRefreshTokenFromRequest(request);
+		if (refreshToken != null && jwtService.validateToken(refreshToken)) {
+			log.info("Access Token 만료, Refresh Token 사용");
+			String username = jwtService.extractUsername(refreshToken);
+			Object role = jwtService.extractRole(refreshToken);
+			String newAccessToken = jwtService.generateAccessToken(username, role);
+			setAuthenticationContext(newAccessToken);
+			jwtService.setHeaderWithAccessToken(response, newAccessToken);
+		} else {
+			sendAuthenticationFailureResponse(response);
+		}
 	}
 
 	private void sendAuthenticationFailureResponse(HttpServletResponse response) throws IOException {
@@ -84,7 +87,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		responseBody.put("message", "Authentication failed");
 
 		// 응답 본문을 JSON 형식으로 변환하여 출력
-		ObjectMapper objectMapper = new ObjectMapper();
 		response.getWriter().write(objectMapper.writeValueAsString(responseBody));
 	}
 
