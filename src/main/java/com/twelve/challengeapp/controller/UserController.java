33fd1d6a535bd2983003c0f4e74e8cecd12c516f
lis0517 <01034895017a@gmail.com>
2024-06-21 11:15:27 +0900

    update Controller
    - Impl을 직접 연결하던 문제 수정

--- src/main/java/com/twelve/challengeapp/controller/UserController.java
+++ src/main/java/com/twelve/challengeapp/controller/UserController.java
@@ -1,16 +1,18 @@
 package com.twelve.challengeapp.controller;
 
-import com.twelve.challengeapp.dto.UserResponseDto;
-import com.twelve.challengeapp.jwt.UserDetailsImpl;
-import com.twelve.challengeapp.util.SuccessResponse;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.twelve.challengeapp.dto.UserRequestDto;
-import com.twelve.challengeapp.service.UserServiceImpl;
+import com.twelve.challengeapp.dto.UserResponseDto;
+import com.twelve.challengeapp.jwt.UserDetailsImpl;
+import com.twelve.challengeapp.service.UserService;
 import com.twelve.challengeapp.util.SuccessResponseFactory;
 
 import jakarta.validation.Valid;
@@ -19,9 +21,9 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/users")
 public class UserController {
 
-	private final UserServiceImpl userService;
+	private final UserService userService;
 
-	public UserController(UserServiceImpl userService) {
+	public UserController(UserService userService) {
 		this.userService = userService;
 	}
 
@@ -38,7 +40,7 @@ public class UserController {
 	}
 
 	@DeleteMapping
-	public ResponseEntity<SuccessResponse<Void>> withdraw(@RequestBody @Valid UserRequestDto.Withdrawal requestDto,
+	public ResponseEntity<?> withdraw(@RequestBody @Valid UserRequestDto.Withdrawal requestDto,
 		@AuthenticationPrincipal UserDetailsImpl userDetails) {
 
 		userService.withdraw(requestDto, userDetails);
