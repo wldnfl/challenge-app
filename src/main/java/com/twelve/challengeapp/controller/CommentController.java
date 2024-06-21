11d66c82552da3712ea25edcbdd1f43e5db07ebc
hanraeul <toast17@naver.com>
2024-06-21 15:30:39 +0900

    ♻️ refactor Comments URI 수정

--- src/main/java/com/twelve/challengeapp/controller/CommentController.java
+++ src/main/java/com/twelve/challengeapp/controller/CommentController.java
@@ -3,7 +3,6 @@ package com.twelve.challengeapp.controller;
 import com.twelve.challengeapp.dto.CommentRequestDto;
 import com.twelve.challengeapp.dto.CommentResponseDto;
 import com.twelve.challengeapp.entity.Comment;
-import com.twelve.challengeapp.entity.Timestamped;
 import com.twelve.challengeapp.jwt.UserDetailsImpl;
 import com.twelve.challengeapp.service.CommentService;
 import com.twelve.challengeapp.util.SuccessResponseFactory;
@@ -19,13 +18,13 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/api/posts/{postId}")
+@RequestMapping("/api/posts")
 public class CommentController {
 
 
     private final CommentService commentService;
 
-    @PostMapping("/comments")
+    @PostMapping("/{postId}/comments")
     public ResponseEntity<?> createComment(@PathVariable Long postId, @RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         Comment comment = commentService.createComment(postId, commentRequestDto.getContent(), userDetails.getUser());
         CommentResponseDto responseDto = new CommentResponseDto(comment.getId(), comment.getContent(), comment.getUser().getUsername(), comment.getCreatedAt(), comment.getUpdatedAt());
@@ -45,7 +44,7 @@ public class CommentController {
         return SuccessResponseFactory.noContent();
     }
 
-    @GetMapping("/comments")
+    @GetMapping("/{postId}/comments")
     public ResponseEntity<?> getCommentsByPostId(@PathVariable Long postId) {
         List<Comment> comments = commentService.getCommentsByPostId(postId);
         List<CommentResponseDto> responseDtos = comments.stream()
