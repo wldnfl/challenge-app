b62449da4b11ed95213a07a076d31e536fc76c9b
lis0517 <01034895017a@gmail.com>
2024-06-23 21:16:13 +0900

    refactor UserRequestDto, UserResponseDto
    테스트 코드 작성 중 오류로 생성자 부분 수정
    정규식 및 길이 관련 상수로 관리되도록 수정

--- src/main/java/com/twelve/challengeapp/dto/UserRequestDto.java
+++ src/main/java/com/twelve/challengeapp/dto/UserRequestDto.java
@@ -4,60 +4,94 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 public class UserRequestDto {
 
+	private static final String USERNAME_MSG = "Username must contain only lowercase letters and numbers";
+	private static final String PASSWORD_MSG = "EditInfo must contain at least one uppercase letter, one lowercase letter, one number, and one special character";
+	private static final String USERNAME_REGEXP = "^[a-z0-9]*$";
+	private static final String PASSWORD_REGEXP = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]*$";
+
+	private static final int USERNAME_MIN = 4;
+	private static final int USERNAME_MAX = 10;
+
+	private static final int PASSWORD_MIN = 8;
+	private static final int PASSWORD_MAX = 15;
+
+
 	@Getter
-	@SuperBuilder
 	private static class UserInfo {
 		@NotBlank
-		@Size(min = 4, max = 10)
-		@Pattern(regexp = "^[a-z0-9]*$", message = "Username must contain only lowercase letters and numbers")
-		private String username;
+		@Size(min = USERNAME_MIN, max = USERNAME_MAX)
+		@Pattern(regexp = USERNAME_REGEXP, message = USERNAME_MSG)
+		private final String username;
 
 		@NotBlank
-		@Size(min = 8, max = 15)
-		@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]*$", message = "EditInfo must contain at least one uppercase letter, one lowercase letter, one number, and one special character")
-		private String password;
+		@Size(min = PASSWORD_MIN, max = PASSWORD_MAX)
+		@Pattern(regexp = PASSWORD_REGEXP, message = PASSWORD_MSG)
+		private final String password;
+
+		public UserInfo(String username, String password) {
+			this.username = username;
+			this.password = password;
+		}
 	}
 
 	@Getter
-	@SuperBuilder
 	public static class Register extends UserInfo {
 
 		@NotBlank
-		private String nickname;
+		private final String nickname;
 		@NotBlank
-		private String introduce;
+		private final String introduce;
 		@Email
-		private String email;
+		private final String email;
+
+		@Builder
+		public Register(String username, String password, String nickname, String introduce, String email) {
+			super(username, password);
+			this.nickname = nickname;
+			this.introduce = introduce;
+			this.email = email;
+		}
 	}
 
 	@Getter
-	@SuperBuilder
 	public static class Login extends UserInfo {
+		@Builder
+		public Login(String username, String password) {
+			super(username, password);
+		}
 	}
+
 	//회원 정보 수정
 	@Getter
+	@Builder
 	public static class EditInfo {
 
 		@NotBlank
-		@Size(min = 8, max = 15)
-		@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]*$", message = "EditInfo must contain at least one uppercase letter, one lowercase letter, one number, and one special character")
+		@Size(min = PASSWORD_MIN, max = PASSWORD_MAX)
+		@Pattern(regexp = PASSWORD_REGEXP, message = PASSWORD_MSG)
 		private String password;
 		@NotBlank
 		private String nickname;
 		@NotBlank
 		private String introduce;
 	}
+
 	//회원 탈퇴
 	@Getter
-	@SuperBuilder
 	public static class Withdrawal extends UserInfo {
+		@Builder
+		public Withdrawal(String username, String password) {
+			super(username, password);
+		}
 	}
-
 
 }
