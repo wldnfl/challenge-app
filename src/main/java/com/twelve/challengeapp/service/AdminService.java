8a82ba744518dc5d8448d864893d737f428544a1
lis0517 <01034895017a@gmail.com>
2024-06-24 18:05:57 +0900

    add post management
    게시물 관리 기능 추가

--- src/main/java/com/twelve/challengeapp/service/AdminService.java
+++ src/main/java/com/twelve/challengeapp/service/AdminService.java
@@ -2,6 +2,8 @@ package com.twelve.challengeapp.service;
 
 import java.util.List;
 
+import com.twelve.challengeapp.dto.PostRequestDto;
+import com.twelve.challengeapp.dto.PostResponseDto;
 import com.twelve.challengeapp.dto.UserRequestDto;
 import com.twelve.challengeapp.dto.UserResponseDto;
 
@@ -18,5 +20,14 @@ public interface AdminService {
 
 	// 유저 삭제
 	void deleteUser(Long userId);
+
+	// 모든 게시물 조회
+	List<PostResponseDto> getAllPosts();
+
+	// 게시물 수정
+	PostResponseDto updatePost(Long postId, PostRequestDto postRequestDto);
+
+	// 게시물 삭제
+	void deletePost(Long postId);
 
 }
